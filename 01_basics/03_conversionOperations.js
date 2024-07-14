01f153342a1220c747e8ef1d67c98d5fa1f58b82
vanshshah31 <157968568+vanshshah31@users.noreply.github.com>
2024-07-14 14:33:24 +0000

    conversion + operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,7 +23,38 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 //copy path first time and right click in terminal is a shortcut
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
 // **************************************************Operations************************************************************
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Hello";
+let str2 = " Vansh";
+let str3 = str1 + str2;
+//console.log(str3);
+// for conversions see ecma script 7.1 type conversion
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 = 4) * 5 % 3);
+
+//see operator precedence as well
+console.log(+true);
+//console.log(true+); gives error
+console.log(+"");
+
+
+let num1 , num2 , num3
+num1=num2=num3 = 2+2
