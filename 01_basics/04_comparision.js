346a08cef02c6303b3d64a72cc9a08d9698e2bff
vanshshah31 <157968568+vanshshah31@users.noreply.github.com>
2024-07-14 20:00:14 +0000

    comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -13,6 +13,10 @@
 
 // The reason is that "==" and "> < >= <=" work differently.Comparisions convert null to a number treating it as 0. So [3] nul >=0 is true and [1] null > 0 is false
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// "===" strict check --> checks also the datatype
+// avoid such conversions
+console.log("2" === 2);
