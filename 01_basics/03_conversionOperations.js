1df5dac5981e5be3f2df1715cea955e6d54e7911
vanshshah31 <157968568+vanshshah31@users.noreply.github.com>
2024-07-14 13:03:05 +0000

    conversion of DataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = " vansh";
+// shift + alt + up/down to copy a line of code up or down
+//console.log(typeof score);
+//console.log(typeof score);
+// let valueinNumber = Number(score);
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => Nan
+true => 1 , false => 0
+*/
+
+let isLoggedIn = "hitesh";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// ""=>  false;
+// "hitesh" => true;
+//copy path first time and right click in terminal is a shortcut
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
