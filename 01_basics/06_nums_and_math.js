869dcaa6a267e4de44af13f54a80fef2be671d27
vanshshah31 <157968568+vanshshah31@users.noreply.github.com>
2024-07-21 04:53:09 +0000

    nums_and_math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,7 +1,28 @@
 const score = 400;
-console.log(score);
+//console.log(score);
 
 const balance = new Number(100);
-console.log(balance);
-console.log(balance.toString());
-console.log(typeof(balance));
+//console.log(balance);
+//console.log(typeof(balance));
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1123.8966;
+//console.log(otherNumber.toPrecision(3)); // imp and little tricky
+
+const hundreds = 1000000;
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++++++++Maths++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(-4.3));
+// console.log(Math.ceil(-4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(-4.3));
+// console.log(Math.floor(4.3));
+
+// console.log(Math.min(4 , 3 , 6 , 8));
+// console.log(Math.max(4 , 3 , 6 , 8));
