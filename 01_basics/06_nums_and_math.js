e4e552772d01b4b6e06f1faa198182a20c46540c
vanshshah31 <157968568+vanshshah31@users.noreply.github.com>
2024-07-21 10:49:58 +0000

    nums_and_math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -25,4 +25,13 @@ const hundreds = 1000000;
 // console.log(Math.floor(4.3));
 
 // console.log(Math.min(4 , 3 , 6 , 8));
-// console.log(Math.max(4 , 3 , 6 , 8));
+// console.log(Math.min(4 , 3 , 6 , 8));
+
+// console.log(Math.random()); // gives value from 0 to 1
+// console.log(Math.random() * 10 + 1); // here +1 ensures that 0 values don't come
+// console.log(Math.floor(Math.random() * 10 )+ 1); Math.floor gives integer
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min);//imp// max-min gives range// 1 added to remove 0 // +min su from 10 min
