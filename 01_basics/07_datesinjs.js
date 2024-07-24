7b4e7c75223e0c1a67d6c7299ad1d2b628df4fb9
vanshshah31 <157968568+vanshshah31@users.noreply.github.com>
2024-07-24 04:03:40 +0000

    datesinjs

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -14,6 +14,16 @@ let myCreatedDateeee = new Date("01-14-2023");
 let myTimStamp = Date.now();
 // console.log(myTimStamp);// for creating quizes
 // console.log(myCreatedDateeee.getTime());
-console.log(Math.floor(Date.now() / 1000)); // DIDNT UNDERSTAND but this technique reduces spaces
+//console.log(Math.floor(Date.now() / 1000)); // DIDNT UNDERSTAND but this technique reduces spaces
 
 let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay() + 21);
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default' , {
+     weekday:"long",
+     })
+     
