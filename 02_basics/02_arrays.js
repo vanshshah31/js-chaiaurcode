de6e17938b4f0ee0bae3b64d5cde51781bbd1a53
vanshshah31 <157968568+vanshshah31@users.noreply.github.com>
2024-07-26 09:41:17 +0000

    02_arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -16,4 +16,12 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
 const real_another_array = another_array.flat(Infinity);
 //console.log(real_another_array);
 
-Array
+console.log(Array.isArray("Vansh"));
+console.log(Array.from("Vansh")); //converts to array
+console.log(Array.from({ name: "Vansh" })); // interesting case
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
