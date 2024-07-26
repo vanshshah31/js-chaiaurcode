cfaef1c154e190c4271bd7186ee470e54be986ef
vanshshah31 <157968568+vanshshah31@users.noreply.github.com>
2024-07-26 10:20:57 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,28 @@
+// singleton created by constructors and not literals
+
+// object literals
+//Object.create   // constructor method
+
+const mySym = Symbol("key1"); //symbol
+
+const JsUser = {
+  name: "Haaland",
+  full_name: "Erling Haaland",
+  [mySym]: "mykey1",
+  age: 24,
+  country: "Norway",
+  jersey_number: 9,
+  email: "haaland@mancity.com",
+  eplwinner: "Manchester city",
+  isGoat: true,
+  training_days: ["Monday", "Wednesday", "Friday", "Saturday"],
+};
+//console.log(JsUser.eplwinner);
+console.log(JsUser["full_name"]);
+console.log(JsUser["email"]);
+console.log(JsUser[mySym]);
+
+JsUser.email = "debruyne@mancity.com"; // overwriting
+Object.freeze(JsUser); // for locking
+JsUser.email = "foden@mancity.com";
+console.log(JsUser);
