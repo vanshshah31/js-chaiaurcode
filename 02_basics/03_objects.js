adbd80ea62126484770a04d6b5833046970e0391
vanshshah31 <157968568+vanshshah31@users.noreply.github.com>
2024-07-29 04:40:41 +0000

    03_objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -18,11 +18,21 @@ const JsUser = {
   training_days: ["Monday", "Wednesday", "Friday", "Saturday"],
 };
 //console.log(JsUser.eplwinner);
-console.log(JsUser["full_name"]);
-console.log(JsUser["email"]);
-console.log(JsUser[mySym]);
+// console.log(JsUser["full_name"]);
+// console.log(JsUser["email"]);
+// console.log(JsUser[mySym]);
 
 JsUser.email = "debruyne@mancity.com"; // overwriting
-Object.freeze(JsUser); // for locking
+//Object.freeze(JsUser); // for locking
 JsUser.email = "foden@mancity.com";
-console.log(JsUser);
+// console.log(JsUser);
+
+JsUser.greeting = function () {
+  console.log("Hello JS user");
+};
+
+JsUser.greetingTwo = function () {
+  console.log(`Hello JS user , ${this.name}`);
+};
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
